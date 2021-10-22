de8b1d762fa13334f3077a171f5cb929c4b5aab9
Joe Scotto <contact@joescotto.com>
2021-10-22 10:38:30 -0400

    Add .closeKeyboardGestures() modifier for View

---
+++ Sources/TextFieldStepper/CloseKeyboardGestures.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+struct CloseKeyboardGestures: ViewModifier {
+    func body(content: Content) -> some View {
+        content.onTapGesture {
+            UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+        }
+        .gesture(
+            // Swipe down gesture
+            DragGesture(minimumDistance: 0, coordinateSpace: .local).onEnded({ gesture in
+                if gesture.translation.height > 0 {
+                    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                }
+        }))
+    }
+}
+
+extension View {
+    func closeKeyboardGestures() -> some View {
+        modifier(CloseKeyboardGestures())
+    }
+}
