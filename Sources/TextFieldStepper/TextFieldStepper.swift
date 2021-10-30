5ffe19b6580160658873ab8a80302e119e52910b
Joe Scotto <contact@joescotto.com>
2021-10-30 11:24:19 -0400

    Attempting internal protection level

--- Sources/TextFieldStepper/TextFieldStepper.swift
+++ Sources/TextFieldStepper/TextFieldStepper.swift
@@ -1,50 +1,5 @@
 import SwiftUI
 
-struct LongPressButton: View {
-    @Binding var double: Double
-
-    @State private var timer: Timer? = nil
-    @State private var isLongPressing = false
-
-    let image: String
-    let config: TextFieldStepperConfig
-    let action: () -> Void
-
-    var body: some View {
-        Button(action: {
-            if (isLongPressing) {
-               invalidateLongPress()
-            } else {
-                action()
-            }
-        }, label: {
-            Image(systemName: image).resizable().aspectRatio(contentMode: .fit)
-        })
-        .frame(height: 35)
-        .simultaneousGesture(LongPressGesture(minimumDuration: 0.25).onEnded { _ in
-            startTimer()
-        })
-        .buttonStyle(PlainButtonStyle())
-    }
-
-    func invalidateLongPress() {
-        isLongPressing = false
-        timer?.invalidate()
-    }
-    
-    func startTimer() {
-        isLongPressing = true
-        timer = Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true, block: { _ in
-            // FIXME: Long press won't start on min or max.
-            if (double > config.minimum && double < config.maximum) {
-                action()
-            } else {
-                invalidateLongPress()
-            }
-        })
-    }
-}
-
 fileprivate extension View {
     func thiccText() -> some View {
         self.multilineTextAlignment(.center)
@@ -58,6 +13,11 @@ fileprivate extension Text {
         self
             .font(.footnote)
             .fontWeight(.light)
+    }
+}
+
+protocol TextFieldStepperConfigProtocol {
+    var minimum: Double { get
     }
 }
 
